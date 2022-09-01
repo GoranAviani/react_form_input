c9dda3ccfbb91ec7b9d5053c8adbdd781c7ead31
goranaviani <goranaviani@gmail.com>
2022-09-01 21:32:28 +0200

    form input

--- src/components/Form1.js
+++ src/components/Form1.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Form1 = () => {
+    const [userInput, setUserInput] = useState()
 
+    const saveUserInput = (e) => {
+        console.log(e.target.value)
+    }
+    const submitForm1 = (e) => {
+        e.preventDefault()
+        console.log(userInput)
 
+    }
     return (
         <>
         form 1
+            <form onSubmit={submitForm1}>
+               <label>Input 1</label>
+                <input type="text" value={userInput} onChange={saveUserInput}/>
+
+
+            </form>
         </>
     )
 }
