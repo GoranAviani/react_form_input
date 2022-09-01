40aa1db20f1b972422b4168159aecc3a0969d2bc
goranaviani <goranaviani@gmail.com>
2022-09-01 21:33:39 +0200

    form input

--- src/components/Form1.js
+++ src/components/Form1.js
@@ -18,8 +18,7 @@ const Form1 = () => {
             <form onSubmit={submitForm1}>
                <label>Input 1</label>
                 <input type="text" value={userInput} onChange={saveUserInput}/>
-
-
+                <button type="submit">Save form 1</button>
             </form>
         </>
     )
