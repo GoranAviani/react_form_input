c00e2436a8b439e3d0f734432de2ac06c7798515
goranaviani <goranaviani@gmail.com>
2022-09-01 21:40:28 +0200

    form input

--- src/components/Form1.js
+++ src/components/Form1.js
@@ -2,10 +2,18 @@ import React, {useState} from "react";
 
 const Form1 = () => {
     const [userInput, setUserInput] = useState()
+        const [userDescription, setDescriptionInput] = useState()
+
 
     const saveUserInput = (e) => {
         console.log(e.target.value)
         setUserInput(e.target.value)
+    }
+
+    const saveDescriptionInput = (e) => {
+             console.log(e.target.value)
+        setUserInput(e.target.value)
+
     }
     const submitForm1 = (e) => {
         e.preventDefault()
@@ -19,8 +27,8 @@ const Form1 = () => {
                <label>Input 1</label>
                 <input type="text" value={userInput} onChange={saveUserInput}/>
 <br/>
-                               <label>Input 1</label>
-                <input type="text" value={userInput} onChange={saveUserInput}/>
+                               <label>Input 2</label>
+                <input type="text" value={descriptionInput} onChange={saveDescriptionInput}/>
                 <button type="submit">Save form 1º</button>
             </form>
         </>
